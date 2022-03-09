f2cf4372164667c22f0474487fa3381f35f71cb5
vineetdas770 <71541344+vineetdas770@users.noreply.github.com>
2022-03-09 19:18:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from multiapp import Multiapp
+from pages import home, filtering, compression, realtime
+import streamlit as st
+
+    
+app = Multiapp()
+
+app.add_app("Home", home.app)
+app.add_app("Object Detection", realtime.app)
+app.add_app("Image Filtering", filtering.app)
+app.add_app("Image Compression", compression.app)
+
+app.run()
